261728f0882c2d0ae3fc2b5c85df6bd02b8ce81f
Srenethesh Mahendren <95108049+srenethesh@users.noreply.github.com>
2023-10-31 12:37:18 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <header className='header'>
+        <nav className='nav'>
+          <ul className='ul'>    
+                  <img id='img' src='https://www.codebuilders.in/img/code-builders-logo.svg' height='50' width='50' alt='Codebuilders'></img>      
+           				<li id='li'><a id='a' href="#home">Home</a></li>
+				          <li id='li'><a id='a' href="#home">Home</a></li>
+				          <li id='li'><a id='a' href="#home">Home</a></li>
+				          <li id='li'><a id='a' href="home">Home</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default App
